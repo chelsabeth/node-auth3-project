cc60c99601bd0d06edb827e95d20f9d3cfb96537
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-29 20:00:55 -0500

    login and can see all uusers

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -12,11 +12,46 @@ router.post("/register", (req, res) => {
     const hash = bcrypt.hashSync(user.password, 10); // hashed a password when registering a user
     user.password = hash;
 
-    Users.add(user)
+    User.add(user)
         .then(newUser => {
-            res.status(201).json(saved);
+            res.status(201).json(newUser);
         })
         .catch(err => {
             res.status(500).json(err);
         });
 });
+
+router.post("/login", (req, res) => {
+    let { username, password } = req.body;
+
+    User.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = signToken(user)
+
+                res.status(200).json({
+                    message: ({token}),
+                });
+            } else {
+                res.status(401).json({ you: "shall not pass" });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+function signToken(user) { 
+    const payload = {
+      user
+    };
+  
+    const options = {
+      expiresIn: '1d'
+    }
+  
+    return jwt.sign(payload, jwtSecret, options)
+  }
+
+module.exports = router;
