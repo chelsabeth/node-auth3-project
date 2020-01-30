329dbf88114ac3a5b606dbbf47f514b4eebbbdd6
Chelsea Wetzel <chelseagilmore14@gmail.com>
2020-01-29 20:57:45 -0500

    attempt stretch

--- users/users-router.js
+++ users/users-router.js
@@ -11,4 +11,14 @@ router.get("/", restricted, (req, res) => {
     .catch(err => res.send(err));
 });
 
+// function onlyDepartment(department) {
+//     return function(req, res, next) {
+//         if (req.user && req.user.department && req.user.department.toLowerCase() === department) {
+//             next();
+//         } else {
+//             res.status(403).json({ sorry: "couldnt get users in your department" })
+//         }
+//     }
+// }
+
 module.exports = router;
